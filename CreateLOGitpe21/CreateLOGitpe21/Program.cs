d9ebd856c6171fa36a74b366b948b2662f672c9e
MarkoMoist <89842536+MarkoMoist@users.noreply.github.com>
2022-02-18 17:27:59 +0200

    Add files via upload

---
+++ CreateLOGitpe21/CreateLOGitpe21/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateLOGitpe21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string newDirectory = "LOGITpe21";
+            CreateMyDirectory(newDirectory);
+        } 
+        
+        private static void CreateMyDirectory(string myDirectoryName)
+        {
+            string rootDirectory = @"C:\Users\marko\Samples";
+            string directoryFullPath = $@"{rootDirectory}/{myDirectoryName}";
+        
+          if (Directory.Exists(directoryFullPath))
+            {
+                Console.WriteLine($"Directory {myDirectoryName} already exsists in {rootDirectory}.");
+            }
+            else
+            {
+                Directory.CreateDirectory(directoryFullPath);
+                Console.WriteLine($"Directory {myDirectoryName} has been created.");
+            }
+        
+        
+        
+        }
+    }
+}
---
+++ CreateLOGitpe21/Myfiles/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myfiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          CreateFilesFromMyList(ReplaceNumbers(GetDataFromFile()));
+          UpdateMyFiles(ReplaceNumbers(GetDataFromFile()));
+        }
+
+        private static string[] GetDataFromFile()
+        {
+            string sourceFilePath = $@"C:\Users\marko\Samples\LOGITpe21\Myfiles.txt";
+            string[] dataFromFile = File.ReadAllLines(sourceFilePath);
+            return dataFromFile;
+        }
+
+        private static  string[] ReplaceNumbers(string[] sourceArray)
+        {
+            for(int i = 0; i < sourceArray.Length; i++)
+            {
+                sourceArray[i] = sourceArray[i].Replace('4', 'a').Replace('3', 'e').Replace('0', 'o').Replace('1', 'i');     
+            }
+
+            return sourceArray;
+        }
+
+        private static void CreateFilesFromMyList(string[] arrayOfNames)
+        {
+            string rootPath = $@"C:\Users\marko\Samples\LOGITpe21";
+
+            for(int i = 0; i < arrayOfNames.Length; i++)
+            {
+                File.Create($@"{rootPath}\{arrayOfNames[i]}.txt");
+            }
+        }
+
+        private static void UpdateMyFiles(string[] updateArray)
+        {
+            string sourceFilePath = $@"C:\Users\marko\Samples\LOGITpe21\Myfiles.txtsss";
+            File.WriteAllLines(sourceFilePath, updateArray);
+        }
+
+    }
+}
